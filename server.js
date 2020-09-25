cf60a7ab4198124f31a6d0fa10987b3c0da295d5
Sergio <sza0210escom@gmail.com>
2020-09-25 13:54:47 -0500

    primer commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+var hbs = require('hbs');
+
+require('./hbs/helpers')
+hbs.registerPartials(__dirname + '/views/partials');
+
+const port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', { nombre: 'sergio' })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.listen(port, () => {
+    console.log('Escuchando el puerto ' + port);
+});
